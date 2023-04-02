bf76acbea3d8624a58e76c6b78b2069d310f2822
Nguyễn Quang Huy <nguyenhuyilc2003@gmail.com>
2023-04-02 12:25:35 +0700

    Hoàn thành Upload Image Nhưng thiếu Truy xuất Image

--- app/src/main/java/edu/poly/instagramcloneapp/fragment/main/home.kt
+++ app/src/main/java/edu/poly/instagramcloneapp/fragment/main/home.kt
@@ -1,30 +1,42 @@
 package edu.poly.instagramcloneapp.fragment.main
 
-// Connect firebase:  https://www.youtube.com/watch?v=YOT8P1PtJQg&t=618s
-//Viewbinding in fragment: https://www.youtube.com/watch?v=v11x54y5YVc
-//SignIn and SignOut : https://www.youtube.com/watch?v=idbxxkF1l6k&list=PLQFUWT9wUMWEXj9AVanMZg1tRUGr95aBr&index=10&t=784s
+
+//Nguồn tham khảo:
+    // Connect firebase:  https://www.youtube.com/watch?v=YOT8P1PtJQg&t=618s
+    //Viewbinding in fragment: https://www.youtube.com/watch?v=v11x54y5YVc
+    //SignIn and SignOut : https://www.youtube.com/watch?v=idbxxkF1l6k&list=PLQFUWT9wUMWEXj9AVanMZg1tRUGr95aBr&index=10&t=784s
+    //Upload Image: https://www.youtube.com/watch?v=GmpD2DqQYVk
+import android.app.Activity
+import android.app.ProgressDialog
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
+
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import edu.poly.instagramcloneapp.MainActivity
+import edu.poly.instagramcloneapp.databinding.ActivityMainBinding
 import edu.poly.instagramcloneapp.databinding.FragmentHomeBinding
 import edu.poly.instagramcloneapp.model.UserModel
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.SimpleFormatter
 
 
 class home : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    //Firbase Image
+        //Image
+        private lateinit var imageUri:Uri
 
-    //Firebase Auth
-    private lateinit var firebaseAuth: FirebaseAuth
     //Firebase Database:
         private lateinit var dbRef: DatabaseReference
 
@@ -44,77 +56,86 @@ class home : Fragment() {
         //Chung của firebase
 
             binding = FragmentHomeBinding.inflate(layoutInflater)
-        //Firebase Auth
+        //Image Firebase
+            binding.Pick.setOnClickListener {
+                pickImage()
+            }
+            binding.Upload.setOnClickListener {
+                UploadImage()
+        }
 
-            firebaseAuth = FirebaseAuth.getInstance()
 
-        //Button
-            binding.Send.setOnClickListener {
-            saveUserData()
-        }
-            binding.SignUp.setOnClickListener {
-            signup()
-        }
-        binding.signIn.setOnClickListener {
-            signin()
-        }
+
 
         //Cần cho onCreateView()
             return binding.root
     }
     //Data for firebase
-    private fun saveUserData(){
-        val email_User = binding.EmailInput.text.toString()
+//    private fun saveUserData(){
+//        val email_User = binding.EmailInput.text.toString()
+//
+//        val user_id = dbRef.push().key!!
+//
+//        // Push to model in UserModel
+//        val users = UserModel(user_id,email_User)
+//
+//        dbRef.child(user_id).setValue(users)
+//
+//    }
+    //Image Firebase
+        private fun UploadImage(){
 
-        val user_id = dbRef.push().key!!
+            val progressDialog = ProgressDialog(requireActivity())
+            progressDialog.setMessage("UploadImage ...")
 
-        // Push to model in UserModel
-        val users = UserModel(user_id,email_User)
+            progressDialog.setCancelable(false)
+            progressDialog.show()
 
-        dbRef.child(user_id).setValue(users)
+
+            //Date Upload
+            val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
+            val now = Date()
+
+            // Tạo thư mục lưu Image
+                val fileName = formatter.format(now)
+                val storageReference = FirebaseStorage.getInstance().getReference("images/$fileName")
+
+            //Commit Upload
+                storageReference.putFile(imageUri)
+                .addOnSuccessListener {
+                    binding.imageView.setImageURI(null)
+                    Toast.makeText(requireActivity(),"SuccessFull Uploaded",Toast.LENGTH_SHORT)
+                    if (progressDialog.isShowing){
+                        progressDialog.dismiss()
+                    }
+                }
+                .addOnFailureListener {
+                    if (progressDialog.isShowing){
+                        progressDialog.dismiss()
+                        Toast.makeText(requireActivity(),"Failed",Toast.LENGTH_SHORT)
+
+                    }
+                }
+        }
+
+        private fun pickImage(){
+
+        val intent = Intent()
+        intent.type = "image/*" //* cho phép tất cả loại ảnh: JPEG,PNG,.....
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        startActivityForResult(intent,100)
 
     }
+    //Support for Image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-    //Login For Firebase
-    private fun signup(){
-        val email_User = binding.EmailInput.text.toString()
-        val password = binding.passwordInput.text.toString()
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK){ //Acitivity is need if this is fragment
 
-        if (email_User.isEmpty() && password.isEmpty()){
-            Toast.makeText(requireActivity(), "No Blank Plz", Toast.LENGTH_SHORT).show()
-        }
-        else{
-            firebaseAuth.createUserWithEmailAndPassword(email_User,password).addOnCompleteListener{
-                if (it.isSuccessful){
-                    Toast.makeText(requireActivity(), "Login Complete", Toast.LENGTH_SHORT).show()
-
-                }
-                else{
-
-                    Toast.makeText(requireActivity(), it.exception.toString(), Toast.LENGTH_SHORT).show()
-                }
-            }
+            imageUri = data?.data!!
+            binding.imageView.setImageURI(imageUri)
         }
     }
-    private fun signin(){
-        val email_User = binding.EmailInput.text.toString()
-        val password = binding.passwordInput.text.toString()
 
-        if (email_User.isEmpty() && password.isEmpty()){
-            Toast.makeText(requireActivity(), "No Blank Plz", Toast.LENGTH_SHORT).show()
-        }
-        else{
-            firebaseAuth.signInWithEmailAndPassword(email_User,password).addOnCompleteListener{
-                if (it.isSuccessful){
-                    Toast.makeText(requireActivity(), "Login Complete", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(requireActivity(), MainActivity::class.java)
-                    startActivity(intent)
-                }
-                else{
-
-                    Toast.makeText(requireActivity(), it.exception.toString(), Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
 }
